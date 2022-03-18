b4a955c72b3ba1b595cd2db39ed17ac731a96429
LeonardBue <91261628+LeonardBue@users.noreply.github.com>
2022-03-18 10:56:22 -0700

    producing first logical results

--- heliGPS_analysis.py
+++ heliGPS_analysis.py
@@ -2,27 +2,29 @@
 ## MPB heli-GPS Analysis
 
 from cgi import test
-from locale import normalize
-import numpy as np
-import pandas as pd
+import affine
+import fiona
 import geopandas as gpd
 import georasters as gr
-import fiona
+from locale import normalize
 import matplotlib.pyplot as plt
-
-# import arcpy
-# aprx = arcpy.mp.ArcGISProject(r"C:\Projects\YosemiteNP\Yosemite.aprx")
-# insertLyr = arcpy.mp.LayerFile(r"C:\Projects\YosemiteNP\LayerFiles\Ranger Stations.lyrx")
-# m = aprx.listMaps("Yosemite National Park")[0]
-# refLyr = m.listLayers("Points of Interest")[0]
-# m.insertLayer(refLyr, insertLyr, "BEFORE")
-# aprx.saveACopy(r"C:\Projects\YosemiteNP\Yosemite_updated.aprx")
+import numpy as np
+import pandas as pd
+import rasterstats as rs
+import rasterio
+from rasterio.plot import plotting_extent
+import rioxarray as rxr
 
 
-# geopandas included map, filtered to just this hemisphere
-GDB_FILENAME = './Data/MPB_AERIAL_SURVEY.gdb'
-SPECIES_FILENAME = './Data/Species_classification_2019/Species_classification_2019_Alberta.tif'
-CRS = 4269
+F_GDB = './Data/MPB_AERIAL_SURVEY.gdb'
+F_SPECIES = './Data/Species_classification_2019/Species_classification_2019_aoi'
+F_AOI = './Data/Areas_of_Interest/Areas_of_Interest.shp'
+F_GRAVEL_ROADS = './Data/Road_Network/_gravelRoads.shp'
+F_PAVED_ROADS = './Data/Road_Network/_pavedRoads.shp'
+F_BUFFER = './Data/MPB_buffer/heliGPS_buffer.shp'
+F_MPB_BUFFERED = './Data/MPB_buffer/heliGPS_species_df'
+
+CRS = 'EPSG:3400'
 
 # %%
 
@@ -33,7 +35,7 @@ CRS = 4269
 #                 (world['continent'] == 'South America')]
 # base = westhem.plot(color='white', edgecolor='black', figsize=(11, 11))
 
-layers = fiona.listlayers(GDB_FILENAME)
+layers = fiona.listlayers(F_GDB)
 heliGPS_layers = sorted([lyr for lyr in layers if lyr.endswith('x')])
 polygons = sorted([lyr for lyr in layers if lyr.endswith('p')])
 heliGPS_last_3_years = heliGPS_layers[-3:]
@@ -41,14 +43,16 @@ heliGPS_last_3_years = heliGPS_layers[-3:]
 heliGPS = None
 for l in heliGPS_last_3_years:
     if heliGPS is None:
-        heliGPS = gpd.read_file(GDB_FILENAME, layer=l)
+        heliGPS = gpd.read_file(F_GDB, layer=l)
     else:
-        survey = gpd.read_file(GDB_FILENAME, layer=l)
+        survey = gpd.read_file(F_GDB, layer=l)
         heliGPS = pd.concat([heliGPS, survey])
         
 heliGPS = heliGPS.to_crs(CRS)
 heliGPS = heliGPS.reset_index()
 heliGPS
+
+# imported data should be of geomerty type point. check with: heliGPS.geom_type.head()
 
 # interactively visualize the data
 # heliGPS.explore()
@@ -64,15 +68,12 @@ print(heliGPS.att_stage.value_counts(ascending=True))
 print(heliGPS.zone.value_counts(ascending=True))
 print(heliGPS.loc[heliGPS['att_stage'] == 'Green'].survyear.value_counts(ascending=True))
 
-# %% Read tree species data
-species = gr.from_file(SPECIES_FILENAME)
-df_species = species.to_geopandas()
-
 # %% 
 # These data points do not have an attack stage (att_stage) assigned to them, thus they will be excluded from further anlysis.
 heliGPS.loc[heliGPS['att_stage'] == ''].explore()
 heliGPS.drop(heliGPS[heliGPS.att_stage  == ''].index, inplace=True)
 # All remaining points are associated either to attack stage "Green" or "Red".
+
 
 # Visualize some statistics of the data
 
@@ -85,3 +86,69 @@ for i, c in enumerate(['longitude', 'latitude']):
     heliGPS.boxplot(column=c, by='att_stage', ax=axes[i])
 fig.suptitle('Spacial Distribution of Red and Green Attack Trees', fontsize=16)
 
+# %% 
+# Create buffer around heli-GPS points
+heliGPS_poly = heliGPS.copy()
+heliGPS_poly["geometry"] = heliGPS.geometry.buffer(, cap_style = 3)
+heliGPS_poly.head()
+heliGPS_poly.to_file(F_BUFFER)
+
+# %% 
+# Read tree species data
+species = []
+gdf_species = None
+for i in [1]:
+    species = rxr.open_rasterio(F_SPECIES + str(i) + '.tif', masked=True).squeeze()
+    species.rio.reproject(CRS)
+#     species = gr.from_file(F_SPECIES + str(i) + '.tif')
+#     gdf = species.to_geopandas()
+#     if gdf_species is None:
+#         gdf_species = gdf
+#     else:
+#         gdf_species = pd.concat([gdf_species, gdf])
+
+# gdf_species.to_crs(CRS)
+# # cleanup: set values of 0, corresponding to no trees, to NAN
+# # gdf_species_no_zeros = gdf_species.where(gdf_species == 0, np.nan)
+# ax = gdf_species.value.plot.hist(bins=max(gdf_species.value.unique()), figsize=(8,8))
+print('finished loading species')
+# %% 
+# extract pixel values for each polygon of the bufferes heli-GPS data
+# bounds = gdf_species.total_bounds
+# north, west = bounds[0:2]
+# xsize = bounds[2] - bounds[0]
+# ysize = bounds[3] - bounds[1]
+# species = rxr.open_rasterio(F_SPECIES + str(i) + '.tif', masked=True).squeeze().rio.reproject(CRS)
+affine=species.rio.transform()
+
+heliGPS_species = rs.zonal_stats(F_BUFFER,
+                                    species.values, # gdf_species_no_zeros.values,
+                                    nodata=0,
+                                    # affine=rasterio.transform.from_origin(north, west, xsize, ysize),
+                                    affine=affine,
+                                    geojson_out=True,
+                                    copy_properties=True,
+                                    stats=['count', 'min', 'mean', 'max', 'median', 'majority'])
+                                
+heliGPS_species_df = gpd.GeoDataFrame.from_features(heliGPS_species)
+
+if heliGPS_species_df.crs is None:
+    heliGPS_species_df.set_crs(CRS)
+else:
+    heliGPS_species_df.to_crs(CRS)
+
+heliGPS_species_df.head()
+
+# save file to GeoJSON and shapefile
+heliGPS_species_df.to_file(F_MPB_BUFFERED + '.shp')
+heliGPS_species_df.to_file(F_MPB_BUFFERED + '.shp')
+
+# %%
+fig, ax = plt.subplots(figsize=(10, 10))
+
+species.plot(ax=ax)
+heliGPS.plot(ax=ax, marker='o', markersize=2, color='red')
+# ax.set_axis_off()
+plt.show()
+
+# %%
